c67a420cfbf0d66d6705769244d651115c5ca5b7
SB-Jr <shrijitbasak@gmail.com>
2020-09-26 16:03:37 +0530

    [Refactor]:Refactored interface module

--- cineledger/ledger/interface/__init__.py
+++ cineledger/ledger/interface/__init__.py
@@ -2,14 +2,17 @@
 This is the interface which handles the user interaction and shows the appropriate results to the user.
 Handles the action that needs to take place depending on the user's arguments passed. 
 """
-# TODO: transfer the file to __init__
 
 import argparse
 from ledger.interface import search_interface
 from ledger.interface import manage_interface
-from rich.console import Console
 
-console = Console() 
+
+_SUBCOMMAND = 'subcommand'
+_interfaces = {
+    search_interface.KEY: search_interface,
+    manage_interface.KEY: manage_interface
+}
 
 def handle_command():
     args = _parse_arguments()
@@ -17,7 +20,7 @@ def handle_command():
 
 def _get_paser():
     parser = argparse.ArgumentParser(prog='ledger')
-    subparsers = parser.add_subparsers(title='Commands', dest = 'subcommand')
+    subparsers = parser.add_subparsers(title='Commands', dest = _SUBCOMMAND)
     search_interface.add_search_parser(subparsers)
     manage_interface.add_tracker_parser(subparsers)    
     return parser
@@ -30,10 +33,7 @@ def _parse_arguments():
 def _perform_action(args):
     # TODO: use *list* of command handlers and pass the args to command handler based subcommand value
     # TODO: do not use if else  
-    if args['subcommand'] is None:
+    if args[_SUBCOMMAND] is None:
         # TODO: handle help
         exit()
-    if search_interface.KEY == args['subcommand']:
-        search_interface.handle_action(args)
-    elif manage_interface.KEY in args['subcommand']:
-        manage_interface.handle_action(args)
+    _interfaces[args[_SUBCOMMAND]].handle_action(args)
